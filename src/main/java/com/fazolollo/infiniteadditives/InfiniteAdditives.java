09c9802af15cea36afe26d13cc1fc239076b5931
fazolollo <joachim@autograf.pl>
2014-11-02 21:18:50 +0100

    src

---
+++ src/main/java/com/fazolollo/infiniteadditives/InfiniteAdditives.java
@@ -0,0 +1,32 @@
+package com.fazolollo.infiniteadditives;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="InfiniteAdditives", name="Infinite Additives", version="1.7.2-1.0")
+
+public class InfiniteAdditives
+{
+    @Mod.Instance("InfiniteAdditives")
+    public static InfiniteAdditives instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}

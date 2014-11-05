c23c88b636e29a0c36f629ebd292c6cae7cc632d
fazolollo <joachim@autograf.pl>
2014-11-05 17:48:58 +0100

    Source directory

--- src/main/java/com/fazolollo/infiniteadditives/InfiniteAdditives.java
+++ src/main/java/com/fazolollo/infiniteadditives/InfiniteAdditives.java
@@ -1,38 +1,45 @@
 package com.fazolollo.infiniteadditives;
 
+import com.fazolollo.infiniteadditives.handler.ConfigurationHandler;
 import com.fazolollo.infiniteadditives.proxy.IProxy;
 import com.fazolollo.infiniteadditives.reference.Reference;
+import com.fazolollo.infiniteadditives.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 
 public class InfiniteAdditives
 {
     @Mod.Instance(Reference.MOD_ID)
     public static InfiniteAdditives instance;
 
-    @SidedProxy(clientSide = "com.fazolollo.infiniteadditives.proxy.ClientProxy", serverSide = "com.fazolollo.infiniteadditives.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
 
+        LogHelper.info("Pre Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
+        LogHelper.info("Initialization Complete!");
 
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialization Complete!");
     }
 }
---
+++ src/main/java/com/fazolollo/infiniteadditives/client/gui/ModGuiConfig.java
@@ -0,0 +1,25 @@
+package com.fazolollo.infiniteadditives.client.gui;
+
+import com.fazolollo.infiniteadditives.handler.ConfigurationHandler;
+import com.fazolollo.infiniteadditives.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+import java.util.List;
+
+public class ModGuiConfig extends GuiConfig
+{
+    public ModGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+
+    }
+
+}
